972df96c52abfed840fa159ff3f315d121764645
amirali-yaghoubi <amirali.yaghoubi.work@gmail.com>
2026-08-25 17:32:36 +0330

    feat: add other basic helpers for semantic analyser

--- src/middle/semantic_analyzer.c
+++ src/middle/semantic_analyzer.c
@@ -2,6 +2,9 @@
 #include "common/arena.h"
 #include <stdbool.h>
 
+
+
+static void push_scope(SemanticContext* ctx);
 void semantic_context_init(SemanticContext* ctx, Arena* a)
 {
     ctx->arena = a;
@@ -9,6 +12,8 @@ void semantic_context_init(SemanticContext* ctx, Arena* a)
     ctx->has_error = false;
     ctx->error_count = 0;
     ctx->warning_count = 0;
+
+    push_scope(ctx); // Creates global scope
 }
 
 
@@ -24,4 +29,59 @@ static void push_scope(SemanticContext* ctx)
 static void pop_scope(SemanticContext* ctx)
 {
     ctx->current_scope = ctx->current_scope->parent;
+}
+
+
+static Symbol* lookup_current(SemanticContext* ctx, const char* name)
+{
+    Symbol* current = ctx->current_scope->symbols;
+
+    while(current != NULL)
+    {
+        if(strcmp(current->name, name) == 0)
+            return current;
+        
+        current = current->next;
+    }
+
+    return NULL;
+}
+
+
+static Symbol* lookup_all(SemanticContext* ctx, const char* name)
+{
+    Scope* scope = ctx->current_scope;
+
+    while(scope != NULL)
+    {
+        Symbol* current = scope->symbols;
+
+        while(current != NULL)
+        {
+          if(strcmp(current->name, name) == 0)
+            return current;
+            
+            current = current->next;
+        }
+        
+        scope = scope->parent;
+    }   
+}
+
+
+static bool add_symbol(SemanticContext* ctx, const char* name, DataType type)
+{
+    if(lookup_current(ctx, name) != NULL)
+        return false;
+
+    Symbol* sym = arena_alloc(ctx->arena, sizeof(Symbol));
+
+    sym->name = name;
+    sym->type = type;
+    sym->is_init = false;
+
+    sym->next = ctx->current_scope->symbols;
+    ctx->current_scope->symbols = sym;
+
+    return true;
 }
